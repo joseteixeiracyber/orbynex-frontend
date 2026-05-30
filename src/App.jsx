152b717e946788e6f29424e008fba51cba5949de
joseteixeiracyber <dev.arturteixeira@gmail.com>
2026-05-30 20:36:22 -0300

    adiciona scroll automatico entre paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import ScrollToTop from "./components/ScrollToTop";
+
 import Home from "./pages/Home";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfUse from "./pages/TermsOfUse";
@@ -7,6 +9,9 @@ import TermsOfUse from "./pages/TermsOfUse";
 export default function App() {
   return (
     <BrowserRouter>
+
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Home />} />
 
@@ -20,6 +25,7 @@ export default function App() {
           element={<TermsOfUse />}
         />
       </Routes>
+
     </BrowserRouter>
   );
 }
